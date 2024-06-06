33e936669a37196e57bd3ed0ef774180bafcfc24
Erolinda Kajtazi <erolindakajtazi@gmail.com>
2024-06-06 11:14:10 +0200

    added some functionality

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,20 @@ function App() {
     })
   }
 
+  function handleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      }
+    });
+  }
+
   let content;
 
   if (projectsState.selectedProjectId === null) {
